4442432ef0e27ca85bb55c5edd558c7f55ff9e74
Albin Remnestål <albin.remnestal@gmail.com>
2018-03-10 15:21:59 +0100

    refactor: run ep.py as an App instance
    Rather than running program as an instance of the main func, the program is run as an instance of the App object.

--- ep.py
+++ ep.py
@@ -2,58 +2,63 @@ import curses
 import sys
 import os
 
-def main(stdscr):
+class App(object):
 
-    # file name required
-    if len(sys.argv) < 2:
-        sys.exit('No file specified.')
+    def start(self, stdscr):
+        self.stdscr = stdscr
+        self.run()
 
-    # Clear screen at startup
-    stdscr.clear()
+    def run(self):
+        # file name required
+        if len(sys.argv) < 2:
+            sys.exit('No file specified.')
 
-    # set up colors
-    curses.start_color()
-    curses.use_default_colors()
-    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+        # Clear screen at startup
+        self.stdscr.clear()
 
-    #  configure runtime vars
-    scr_height, scr_width = stdscr.getmaxyx()
+        # set up colors
+        curses.start_color()
+        curses.use_default_colors()
+        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_BLACK)
+        curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
-    # show splash screen
-    __splash(stdscr, scr_height, scr_width)
-    stdscr.getkey()
-    stdscr.clear()
+        #  configure runtime vars
+        scr_height, scr_width = self.stdscr.getmaxyx()
 
-    # load the contents of the file
-    contents = __readfile(sys.argv[1])
-    line_marign = len(str(len(contents))) + 2
-    for index, line in enumerate(contents):
-        stdscr.addstr(index, 1, str(index+1))
-        stdscr.addstr(index, line_marign, line)
+        # show splash screen
+        self.__splash(self.stdscr, scr_height, scr_width)
+        self.stdscr.getkey()
+        self.stdscr.clear()
 
-    # show screen and require user input
-    stdscr.refresh()
-    stdscr.getkey()
+        # load the contents of the file
+        contents = self.__readfile(sys.argv[1])
+        line_marign = len(str(len(contents))) + 2
+        for index, line in enumerate(contents):
+            self.stdscr.addstr(index, 1, str(index+1))
+            self.stdscr.addstr(index, line_marign, line)
 
-def __readfile(path=''):
-    """ read the contents of a file as a list of lines """
-    if os.path.isfile(path):
-        return open(path).read().splitlines()
-    else:
-        return list()
+        # show screen and require user input
+        self.stdscr.refresh()
+        self.stdscr.getkey()
 
-def __splash(stdscr, height, width):
-    offset_y = int((height-20)/2)
-    offset_x = int((width-60)/2)
-    with open('flag') as fp:
-        for y, line in enumerate(fp):
-            for x, char in enumerate(line):
-                if char == '\n': stdscr.addstr(y + offset_y, x + offset_x, '\n')
-                elif char == '@': stdscr.addstr(y + offset_y, x + offset_x, ' ', curses.color_pair(1))
-                elif char == '%': stdscr.addstr(y + offset_y, x + offset_x, '#', curses.color_pair(2))
-                elif char == ' ': stdscr.addstr(y + offset_y, x + offset_x, '%', curses.color_pair(3))
+    def __readfile(self, path=''):
+        """ read the contents of a file as a list of lines """
+        if os.path.isfile(path):
+            return open(path).read().splitlines()
+        else:
+            return list()
+
+    def __splash(self, stdscr, height, width):
+        offset_y = int((height-20)/2)
+        offset_x = int((width-60)/2)
+        with open('flag') as fp:
+            for y, line in enumerate(fp):
+                for x, char in enumerate(line):
+                    if char == '\n': self.stdscr.addstr(y + offset_y, x + offset_x, '\n')
+                    elif char == '@': self.stdscr.addstr(y + offset_y, x + offset_x, ' ', curses.color_pair(1))
+                    elif char == '%': self.stdscr.addstr(y + offset_y, x + offset_x, '#', curses.color_pair(2))
+                    elif char == ' ': self.stdscr.addstr(y + offset_y, x + offset_x, '%', curses.color_pair(3))
 
 if __name__ == "__main__":
-    curses.wrapper(main)
+    curses.wrapper(App().start)
